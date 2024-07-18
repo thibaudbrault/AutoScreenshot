01667baeb2e12840a5bb2ddfe841c7a0d76c6545
Thibaud BRAULT <thibaud.brault@gide.net>
2024-07-18 15:59:46 +0200

    :recycle: update puppeteer.launch params

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const urls = [
 const bucketName = process.env.AWS_BUCKET_NAME;
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], ignoreDefaultArgs: ['--disable-extensions'] });
   for (const url of urls) {
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080})
